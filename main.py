4b0746aab78fdcde7540ae7fe43088d422f8559a
Christian <christian.facchini@gmail.com>
2015-10-06 22:20:33 +0100

    Split in multiple templates

--- main.py
+++ main.py
@@ -12,25 +12,29 @@ def stops():
 
     lat = request.form['lat']
     lon = request.form['lon']
+
     stops = tfl_api.getStopList(lat, lon)
 
     stopcodes = [s['StopCode1'] for s in stops]
     busses = tfl_api.getBusList(stopcodes)
 
-    text = []
+    results = []
     for stop in stops:
-        text.append('Stop {name} ({letter}):'\
-            .format(name=stop['StopPointName'], letter=stop['StopPointIndicator']))
-
+        result = {
+            'name': '{name} ({letter})'.format(name=stop['StopPointName'], letter=stop['StopPointIndicator']),
+            'busses': []
+        }
         busses_for_stopcode = [b for b in busses if b['StopCode1'] == stop['StopCode1']]
         for bus in sorted(busses_for_stopcode, key=lambda i: i['EstimatedTime']):
-            text.append('{line} towards {destination} should arrive at {time}'\
-                .format(line=bus['LineName'], destination=bus['DestinationText'],
-                        time=U.format_time(bus['EstimatedTime'])))
+            result['busses'].append({
+                'number': bus['LineName'],
+                'direction': bus['DestinationText'],
+                'eta': U.format_time(bus['EstimatedTime'])
+            })
 
-    return '<br/>'.join(text)
+        results.append(result)
 
-
+    return render_template('results.html', results=results)
 
 @app.route("/")
 def main():
