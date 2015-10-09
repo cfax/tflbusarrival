b156058e9690a97714a21a7e93ebfcf1c6ca43e5
Christian <christian.facchini@gmail.com>
2015-10-09 22:44:30 +0100

    Try read the postcode if we have no coordinates

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
+from werkzeug.exceptions import BadRequestKeyError
 from tfl_api import TflApi
+from geo import Geo
 from utilities import Utilities as U
 
 app = Flask(__name__)
@@ -10,8 +12,18 @@ def stops():
     """Retrieve list of bus stops (type: STBC) around the user's location"""
     tfl_api = TflApi()
 
-    lat = request.form['lat']
-    lon = request.form['lon']
+    try:
+        lat = request.form['lat']
+        lon = request.form['lon']
+    except BadRequestKeyError:
+        try:
+            postcode = request.form['postcode']
+        except BadRequestKeyError:
+            return render_template('main.html')  # bail out
+
+        # Convert postcode to coordinates
+        geo = Geo(postcode)
+        lat, lon = geo.getLatitudeAndLongitude()
 
     stops = tfl_api.getStopList(lat, lon)
 
